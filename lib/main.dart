712acc803fc448464b980b1908ea379720d9aa1c
Muhammad Uzair Khan <uzair_khan2012@hotmail.com>
2021-10-26 00:40:54 +0500

    Added the first Sign up page layout

--- lib/main.dart
+++ lib/main.dart
@@ -45,85 +45,93 @@ class _HomeState extends State<Home> {
                   child: Column(
                     children: <Widget>[
                       const SizedBox(
-                        height: 80,
+                        height: 10,
                       ),
-                      const TextField(
+                      InkWell(
+                          child: const Text('Sign Up',
+                            style: TextStyle(
+                              color: Color(0xff1ba1a5),
+                              decoration: TextDecoration.underline,
+                              fontSize: 20,
+                            ),
+                          ),
+                          onTap: () => {}
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      TextField(
                         textAlign: TextAlign.center,
                         decoration: InputDecoration.collapsed(
-                          hintText: 'Username',
+                          hintText: 'Username'.toUpperCase(),
+                          hintStyle: const TextStyle(color: Color(0xffC4C4C4))
                         ),
                       ),
                       const Padding(
                         padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
                         child: Divider(
-                          color: Colors.grey,
+                          color: Color(0xffC4C4C4),
                           height:25,
                           thickness: 2,
                           indent:5,
                           endIndent: 5,
                         ),
                       ),
-                      const TextField(
+                      TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Email'.toUpperCase(),
+                          hintStyle: const TextStyle(color: Color(0xffC4C4C4))
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                        child: Divider(
+                          color: Color(0xffC4C4C4),
+                          height:25,
+                          thickness: 2,
+                          indent:5,
+                          endIndent: 5,
+                        ),
+                      ),
+                      TextField(
                         obscureText: true,
                         textAlign: TextAlign.center,
                         decoration: InputDecoration.collapsed(
-                          hintText: 'Password',
+                          hintText: 'Password'.toUpperCase(),
+                          hintStyle: const TextStyle(color: Color(0xffC4C4C4))
                         ),
                       ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                        child: Divider(
+                          color: Color(0xffC4C4C4),
+                          height:25,
+                          thickness: 2,
+                          indent:5,
+                          endIndent: 5,
+                        ),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Confirm Password'.toUpperCase(),
+                          hintStyle: const TextStyle(color: Color(0xffC4C4C4))
+                        ),
+                      ),
+                      const SizedBox(height: 20),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
                         child: ElevatedButton(
                           onPressed: () => {},
-                          child: const Text ('Login'),
+                          child: Text ('SIGN IN'.toUpperCase()),
                           style: ElevatedButton.styleFrom(
                             primary: const Color(0xff1ba1a5),
                             minimumSize: const Size(150, 40)
                           ),
                         ),
                       ),
-                      InkWell(
-                        child: const Text('Forgot Password?',
-                          style: TextStyle(
-                            color: Color(0xff1ba1a5),
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                        onTap: () => {}
-                      ),
-                      const SizedBox(height: 10),
-                      InkWell(
-                          child: const Text('Create Account?',
-                          style: TextStyle(
-                            color: Color(0xff1ba1a5),
-                            decoration: TextDecoration.underline,
-                          ),
-                          ),
-                          onTap: () => {}
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      SignInButton(
-                        Buttons.Apple,
-                        text: "Sign in with Apple",
-                        onPressed: () {},
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      SignInButton(
-                        Buttons.FacebookNew,
-                        text: "Sign in with Facebook",
-                        onPressed: () {},
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      SignInButton(
-                        Buttons.GoogleDark,
-                        text: "Sign in with Google",
-                        onPressed: () {},
-                      )
                     ],
                   ),
                 ),
