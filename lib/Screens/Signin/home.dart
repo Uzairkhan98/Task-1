94ee4bb1ed41882cae2910ff07be0c1d5297c6eb
Muhammad Uzair Khan <uzair_khan2012@hotmail.com>
2021-11-12 14:38:14 +0500

    Added basic validation to signin page

--- lib/Screens/Signin/home.dart
+++ lib/Screens/Signin/home.dart
@@ -5,134 +5,154 @@ import '../Signup/home.dart';
 import 'reset.dart';
 import '../../Widgets/topbar.dart';
 
+import '../../Mixins/inputvalidation.dart';
 
-class Signin extends StatefulWidget {
+class Signin extends StatefulWidget{
   const Signin({Key? key}) : super(key: key);
 
   @override
   _SigninState createState() => _SigninState();
 }
 
-class _SigninState extends State<Signin> {
+class _SigninState extends State<Signin>  with InputValidationMixin{
+  final formGlobalKey = GlobalKey < FormState > ();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          const expansionWidget(),
-          const SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            flex: 75,
-            child: Container(
-              color: Colors.white70,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(40,0,40,0),
-                  child: Column(
-                    children: <Widget>[
-                      const SizedBox(
-                        height: 80,
-                      ),
-                      const TextField(
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration.collapsed(
-                          hintText: 'Username',
+      body: Form(
+        key: formGlobalKey,
+        child: Column(
+          children: <Widget>[
+            const expansionWidget(),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              flex: 75,
+              child: Container(
+                color: Colors.white70,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(40,0,40,0),
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(
+                          height: 80,
                         ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
-                        child: Divider(
-                          color: Colors.grey,
-                          height:25,
-                          thickness: 2,
-                          indent:5,
-                          endIndent: 5,
-                        ),
-                      ),
-                      const TextField(
-                        obscureText: true,
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration.collapsed(
-                          hintText: 'Password',
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                        child: ElevatedButton(
-                          onPressed: () => {},
-                          child: const Text ('Login'),
-                          style: ElevatedButton.styleFrom(
-                              primary: const Color(0xff1ba1a5),
-                              minimumSize: const Size(150, 40)
+                        const TextField(
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Username',
                           ),
                         ),
-                      ),
-                      InkWell(
-                          child: const Text('Forgot Password?',
-                            style: TextStyle(
-                              color: Color(0xff1ba1a5),
-                              decoration: TextDecoration.underline,
+                        const Padding(
+                          padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                          child: Divider(
+                            color: Colors.grey,
+                            height:25,
+                            thickness: 2,
+                            indent:5,
+                            endIndent: 5,
+                          ),
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration.collapsed(
+                            hintText: 'Password',
+                          ),
+                          validator: (password) {
+                            if (isPasswordValid(password!)) {
+                              return null;
+                            } else {
+                              return 'Enter a valid password';
+                            }
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                          child: ElevatedButton(
+                            onPressed: () => {
+                            if (formGlobalKey.currentState!.validate()) {
+                              // If the form is valid, display a snackbar. In the real world,
+                              // you'd often call a server or save the information in a database.
+                            print("Form is valid")
+                        }
+                            },
+                            child: const Text ('Login'),
+                            style: ElevatedButton.styleFrom(
+                                primary: const Color(0xff1ba1a5),
+                                minimumSize: const Size(150, 40)
                             ),
                           ),
-                          onTap: () => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return const Reset();
-                              }),
-                            )
-                          },
-                      ),
-                      const SizedBox(height: 10),
-                      InkWell(
-                          child: const Text('Create Account?',
-                            style: TextStyle(
-                              color: Color(0xff1ba1a5),
-                              decoration: TextDecoration.underline,
+                        ),
+                        InkWell(
+                            child: const Text('Forgot Password?',
+                              style: TextStyle(
+                                color: Color(0xff1ba1a5),
+                                decoration: TextDecoration.underline,
+                              ),
                             ),
-                          ),
-                          onTap: () => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return const Signup();
-                              }),
-                            )
-                          },
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      SignInButton(
-                        Buttons.Apple,
-                        text: "Sign in with Apple",
-                        onPressed: () {},
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      SignInButton(
-                        Buttons.FacebookNew,
-                        text: "Sign in with Facebook",
-                        onPressed: () {},
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      SignInButton(
-                        Buttons.GoogleDark,
-                        text: "Sign in with Google",
-                        onPressed: () {},
-                      )
-                    ],
+                            onTap: () => {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) {
+                                  return const Reset();
+                                }),
+                              )
+                            },
+                        ),
+                        const SizedBox(height: 10),
+                        InkWell(
+                            child: const Text('Create Account?',
+                              style: TextStyle(
+                                color: Color(0xff1ba1a5),
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                            onTap: () => {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) {
+                                  return const Signup();
+                                }),
+                              )
+                            },
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        SignInButton(
+                          Buttons.Apple,
+                          text: "Sign in with Apple",
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        SignInButton(
+                          Buttons.FacebookNew,
+                          text: "Sign in with Facebook",
+                          onPressed: () {},
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        SignInButton(
+                          Buttons.GoogleDark,
+                          text: "Sign in with Google",
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
