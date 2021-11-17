b3dc595760be287803d06932322592cf915bfc14
Muhammad Uzair Khan <uzair_khan2012@hotmail.com>
2021-11-17 13:03:07 +0500

    Added basic template for the dashboard form

--- lib/Screens/Home/dashboard.dart
+++ lib/Screens/Home/dashboard.dart
@@ -12,6 +12,15 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  final formGlobalKey = GlobalKey < FormState > ();
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController fullnameController = TextEditingController();
+  TextEditingController dobController = TextEditingController();
+  TextEditingController countryController = TextEditingController();
+  TextEditingController genderController = TextEditingController();
+  TextEditingController usertypeController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     print('coming here');
@@ -20,18 +29,165 @@ class _DashboardState extends State<Dashboard> {
     print('cpoming here');
     print(dat);
     return Scaffold(
-      body: ListView.builder(
-      itemCount: widget.userData.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(dat.created_at),
-          // When a user taps the ListTile, navigate to the DetailScreen.
-          // Notice that you're not only creating a DetailScreen, you're
-          // also passing the current todo through to it.
+      body: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 10,
+          ),
+      Form(
+        key: formGlobalKey,
+        child: Container(
+          color: Colors.white70,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(40,0,40,0),
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  InkWell(
+                      child: const Text('User Details',
+                        style: TextStyle(
+                          color: Color(0xff1ba1a5),
+                          decoration: TextDecoration.underline,
+                          fontSize: 20,
+                        ),
+                      ),
+                      onTap: () => {}
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: usernameController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Username: ${dat.username}',
+                    ),
+                    validator: (email) { },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                    child: Divider(
+                      color: Color(0xffC4C4C4),
+                      height:25,
+                      thickness: 2,
+                      indent:5,
+                      endIndent: 5,
+                    ),
+                  ),
 
-        );
-      },
-    ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: fullnameController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Full Name: ${dat.full_name}',
+                    ),
+                    validator: (email) { },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                    child: Divider(
+                      color: Color(0xffC4C4C4),
+                      height:25,
+                      thickness: 2,
+                      indent:5,
+                      endIndent: 5,
+                    ),
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: dobController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Date of Birth: ${dat.dob}',
+                    ),
+                    validator: (email) { },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                    child: Divider(
+                      color: Color(0xffC4C4C4),
+                      height:25,
+                      thickness: 2,
+                      indent:5,
+                      endIndent: 5,
+                    ),
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: countryController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Country: ${dat.country}',
+                    ),
+                    validator: (email) { },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                    child: Divider(
+                      color: Color(0xffC4C4C4),
+                      height:25,
+                      thickness: 2,
+                      indent:5,
+                      endIndent: 5,
+                    ),
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: genderController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Gender: ${dat.gender}',
+                    ),
+                    validator: (email) { },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                    child: Divider(
+                      color: Color(0xffC4C4C4),
+                      height:25,
+                      thickness: 2,
+                      indent:5,
+                      endIndent: 5,
+                    ),
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    controller: usertypeController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'User Type: ${dat.user_type}',
+                    ),
+                    validator: (email) { },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric( horizontal: 30, vertical: 0),
+                    child: Divider(
+                      color: Color(0xffC4C4C4),
+                      height:25,
+                      thickness: 2,
+                      indent:5,
+                      endIndent: 5,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                    child: ElevatedButton(
+                      onPressed: () =>  { },
+                      child: Text ('Update'.toUpperCase()),
+                      style: ElevatedButton.styleFrom(
+                          primary: const Color(0xff1ba1a5),
+                          minimumSize: const Size(150, 40)
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+        ],
+      )
     );
   }
 }
